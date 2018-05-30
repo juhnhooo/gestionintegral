88cbe07f8f286892a6dd532a8d78c235b61d7f23
Juhn_Hoo <Juhn_Hoo@Sito-10>
2018-05-30 10:53:27 +0200

    Version Gestion 1.2

--- GestionIntegral/src/gestionintegral/Gestion.java
+++ GestionIntegral/src/gestionintegral/Gestion.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class Gestion {
 	public static void main(String[] args) {
-		System.out.println("Gestion Integral 1.1");
+		System.out.println("Gestion Integral 1.2");
 	}
 }
