8cab7ba88b7ce5c49feecac2a38282ba9d8ba754
Juhn_Hoo <Juhn_Hoo@Sito-10>
2018-05-30 11:19:44 +0200

    Versión ventas 1.1

--- GestionIntegral/src/gestionintegral/ventas.java
+++ GestionIntegral/src/gestionintegral/ventas.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class ventas {
 	public static void main(String[] args) {
-		System.out.println("Gestion ventas 1.0");
+		System.out.println("Gestion ventas 1.1");
 	}
 }
