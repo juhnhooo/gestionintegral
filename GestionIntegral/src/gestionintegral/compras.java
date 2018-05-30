d4129bfb701c34279bca1da53a3134ae722e7bad
Juhn_Hoo <Juhn_Hoo@Sito-10>
2018-05-30 11:20:18 +0200

    Version compras 1.1

--- GestionIntegral/src/gestionintegral/compras.java
+++ GestionIntegral/src/gestionintegral/compras.java
@@ -2,6 +2,6 @@ package gestionintegral;
 
 public class compras {
 	public static void main(String[] args) {
-		System.out.println("Gestion compras 1.0");
+		System.out.println("Gestion compras 1.1");
 	}
 }
